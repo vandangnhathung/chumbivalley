cf3b18736082b31bf0c6f4febbf83e78293a91ad
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2023-11-17 13:39:00 +0700

    feat: javascript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Banner section
 const parallax = (target, speed = 1, cb = () => {
 }) => {
 
@@ -37,3 +38,57 @@ document.querySelectorAll(".cb-forest_black").forEach((element) => {
         element.style.transform = `translateY(${-parallaxValue}%)`;
     });
 });
+
+// Main section
+
+// const convertImageScroll = (el, itemHeight, index) => {
+//     const cbImages = document.querySelector(".cb-images");
+//
+//
+//     const cbImg = document.querySelector(".cb-img");
+//     const cbImgStyle = window.getComputedStyle(cbImg);
+//     const cbImgTopString = cbImgStyle.getPropertyValue('top').split("px")[0];
+//     const cbImgTopNumber = Number(cbImgTopString);
+//
+//     const singleImageHeight = document.querySelector(".cb-img-1").clientHeight;
+//
+//     let isGreaterItemHeight = false;
+//     let isLessItemHeight = true;
+//
+//     const imageItem = document.querySelectorAll(".cb-img-item");
+//
+//     window.addEventListener("scroll", () => {
+//             // console.log(index)
+//
+//             if(isGreaterItemHeight || !isLessItemHeight){
+//                 return;
+//             }
+//
+//             let rectImages = cbImages.getBoundingClientRect();
+//
+//             if(rectImages.top < 0){
+//                 let convertImagePoint = cbImgTopNumber + (rectImages.top * -1) + singleImageHeight;
+//                 console.log(convertImagePoint)
+//                 if(convertImagePoint >= itemHeight && isGreaterItemHeight === false){
+//                     console.log("isGreaterItemHeight: ", isGreaterItemHeight, index + 1)
+//                     imageItem.forEach(el => {
+//                         el.classList.remove("active");
+//                     })
+//                     imageItem[index + 1].classList.add("active");
+//                     isGreaterItemHeight = true;
+//                 }
+//             }
+//         }
+//     )
+// }
+//
+// let totalContentHeight = 0;
+// document.querySelectorAll(".cb-content-item").forEach((el, i) => {
+//     totalContentHeight += el.clientHeight;
+//     convertImageScroll(el, totalContentHeight, i);
+// })
+
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+    console.log(document.querySelector("body").offsetHeight)
+})
